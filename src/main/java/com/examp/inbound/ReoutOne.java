4fbb229e0e89ff137e60680a4a01b1179787499a
Azimeraw <azimerawsoftware@gmail.com>
2023-12-28 09:28:44 +0300

    add jaxb formatter

--- src/main/java/com/examp/inbound/ReoutOne.java
+++ src/main/java/com/examp/inbound/ReoutOne.java
@@ -3,10 +3,18 @@ package com.examp.inbound;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
-
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 @Component
 @Slf4j
 public class ReoutOne extends RouteBuilder {
+    public static JaxbDataFormat jaxbFormat(Class<?> clazz) {
+        try {
+            return new JaxbDataFormat(JAXBContext.newInstance(clazz));
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+}}
     @Override
     public void configure() throws Exception {
         from("direct:start")
